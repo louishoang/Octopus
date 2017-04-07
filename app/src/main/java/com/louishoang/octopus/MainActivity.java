8485b45aea47a0d99d636d90af85eb887e858713
Louis Hoang <bawsing@gmail.com>
2017-04-07 10:54:39 -0700

    Add customAdapter and populate singleTentacle view

--- app/src/main/java/com/louishoang/octopus/MainActivity.java
+++ app/src/main/java/com/louishoang/octopus/MainActivity.java
@@ -1,12 +1,23 @@
 package com.louishoang.octopus;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -22,7 +33,34 @@ public class MainActivity extends AppCompatActivity {
 
     mTentaclesListView = (ListView)findViewById(R.id.tentacles_list_view);
     mTentacleList = TentacleList.get().getTentacles();
-    mAdapter = new ArrayAdapter<Tentacle>(this, R.layout.single_tentacle, R.id.tentacle_name, mTentacleList);
+    mAdapter = new TenTacleAdapter(this, mTentacleList);
     mTentaclesListView.setAdapter(mAdapter);
+  }
+
+  private class TenTacleAdapter extends ArrayAdapter<Tentacle>{
+    public TenTacleAdapter(@NonNull Context context, ArrayList<Tentacle> tentacles) {
+      super(context, 0, tentacles);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+      // Get the data for this position
+      Tentacle tentacle = TentacleList.get().getTentacles().get(position);
+
+
+      //Check if an existing view is being used. otherwise inflate a view.
+      if(convertView == null){
+        convertView = LayoutInflater.from(getContext()).inflate(R.layout.single_tentacle, parent, false);
+      }
+      // Populate data
+      TextView tentacleName = (TextView)convertView.findViewById(R.id.tentacle_name);
+      tentacleName.setText(tentacle.getName());
+
+      TextView tentacleStatus = (TextView)convertView.findViewById(R.id.tentacle_is_on);
+      tentacleStatus.setText(tentacle.getOn().toString());
+
+      return convertView;
+    }
   }
 }
